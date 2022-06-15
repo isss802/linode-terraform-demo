98820ceb8913ce144116d48aef52db36ccb187f4
Hiroyuki Sato <amdsato@gmail.com>
2022-06-15 18:33:25 +0900

    edit database

--- wordpress-managed-db/database.tf
+++ wordpress-managed-db/database.tf
@@ -1,10 +1,15 @@
 resource "linode_database_mysql" "db" {
-	count = var.node_count
-	label = "database"
-	engine_id = var.db.engine_id
-	region = var.region
-	type = var.db.type
-	cluster_size = 1
-	
-	allow_list = ["192.168.128.0/17"]
+  label = "database"
+  engine_id = var.db.engine_id
+  region = var.region
+  type = var.db.type
+  cluster_size = 1
+}
+
+resource "linode_database_access_controls" "db" {
+  count = var.node_count
+  database_id = linode_database_mysql.db.id
+  database_type = "mysql"
+
+  allow_list = ["${element(linode_instance.wordpress.*.private_ip_address, count.index)}/32"]
 }
